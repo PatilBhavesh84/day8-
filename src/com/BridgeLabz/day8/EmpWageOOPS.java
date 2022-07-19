e76683dee595b3cf8882a49cf86c33f5976a2397
PatilBhavesh84 <107094696+PatilBhavesh84@users.noreply.github.com>
2022-07-19 11:23:22 +0530

    Update EmpWageOOPS.java

--- src/com/BridgeLabz/day8/EmpWageOOPS.java
+++ src/com/BridgeLabz/day8/EmpWageOOPS.java
@@ -4,8 +4,8 @@ public class EmpWageOOPS {
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to Employee Wage Computation"
-				+ "");
+		System.out.println("Welcome to Employee Wage Computation");
+			
 
 	}
 }
