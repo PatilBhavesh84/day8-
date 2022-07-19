4cfae83e42e006c18b6c8f794e88acada743a002
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-19 15:41:40 +0530

    Added:Max hours in privious UC

--- src/com/BridgeLabz/day8/EmpWageOOPS.java
+++ src/com/BridgeLabz/day8/EmpWageOOPS.java
@@ -5,11 +5,15 @@ public class EmpWageOOPS {
 	public static final int FULL_TIME=2;
 	public static final int PART_TIME=1;
 	public static final int WORKING_DAY=20;
+	public static final int MAX_HRS=100;
 	EmpWageOOPS(){
 		int empRatePerHrs=20;
 		int totalSalary=0;
 		int salary,empHrs;
-		for(int i=1;i<=WORKING_DAY;i++){ 
+		int totalEmpHrs=0;
+		int totalWorkingDays=0;
+
+		while(totalEmpHrs<=MAX_HRS && totalWorkingDays<=WORKING_DAY) {
 
 			int empCheck = (int)Math.floor(Math.random()*10)%3;
 
@@ -18,13 +22,14 @@ public class EmpWageOOPS {
 				empHrs = 8;
 				break;
 			case PART_TIME:
-				empHrs = 4;
-				break;
+				empHrs = 4;			break;
 			default:
 				empHrs =0;
 			}
 			salary = empRatePerHrs*empHrs;
 			totalSalary = totalSalary + salary;
+			totalEmpHrs= totalEmpHrs+empHrs;
+			totalWorkingDays++;
 		}
 		System.out.println("Monthly Wage is "+ totalSalary);
 	}
