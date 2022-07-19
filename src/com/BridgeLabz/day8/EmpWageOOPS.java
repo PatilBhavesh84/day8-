1cabc88c82daca498cd0dc144e39f86ee5b4cc13
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-19 15:11:34 +0530

    Added:Monthly wage

--- src/com/BridgeLabz/day8/EmpWageOOPS.java
+++ src/com/BridgeLabz/day8/EmpWageOOPS.java
@@ -2,33 +2,37 @@ package com.BridgeLabz.day8;
 
 public class EmpWageOOPS {
 
-	public static final int FULL_TIME=1;
-	public static final int PART_TIME=2;
-	EmpWageOOPS(int p){
-		int ratePerHr=20;
-		int salary;
-		int empHr;
+	public static final int FULL_TIME=2;
+	public static final int PART_TIME=1;
+	public static final int WORKING_DAY=20;
+	EmpWageOOPS(){
+		int empRatePerHrs=20;
+		int totalSalary=0;
+		int salary,empHrs;
+		for(int i=1;i<=WORKING_DAY;i++){ 
 
-		switch(p)  {
-		case FULL_TIME:
-			empHr=8;
-			break;
+			int empCheck = (int)Math.floor(Math.random()*10)%3;
 
-		case PART_TIME: 
-			empHr=4;
-			break;
-
-		default:
-			empHr=0;
-
+			switch(empCheck)	{
+			case FULL_TIME:
+				empHrs = 8;
+				break;
+			case PART_TIME:
+				empHrs = 4;
+				break;
+			default:
+				empHrs =0;
+			}
+			salary = empRatePerHrs*empHrs;
+			totalSalary = totalSalary + salary;
 		}
-		salary=ratePerHr*empHr;
-		System.out.println("Salary is "+salary);
+		System.out.println("Monthly Wage is "+ totalSalary);
 	}
+
 
 	public static void main(String[]args) {
 		System.out.println("Welcome to Employee Wage Computation");
-		int empCheck=(int)Math.floor(Math.random()*10%3);
-		EmpWageOOPS obj = new EmpWageOOPS(empCheck);
+
+		EmpWageOOPS obj = new EmpWageOOPS();
 	}
 }
