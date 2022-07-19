35b7f1302b369dda794e18cbabb71c7d0d2cf437
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-19 11:16:52 +0530

    Added:Welcome messsage

---
+++ src/com/BridgeLabz/day8/EmpWageOOPS.java
@@ -0,0 +1,11 @@
+package com.BridgeLabz.day8;
+
+public class EmpWageOOPS {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Employee Wage Computation"
+				+ "");
+
+	}
+}
