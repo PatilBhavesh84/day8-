00a7b9135e9c6b874223c1fcd48b8072d8396b42
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-27 12:13:47 +0530

    Added:Multiple company wage

--- src/com/BridgeLabz/day8/EmpWageOOPS.java
+++ src/com/BridgeLabz/day8/EmpWageOOPS.java
@@ -1,42 +1,33 @@
 package com.BridgeLabz.day8;
 
 public class EmpWageOOPS {
+	public static final int IS_FULL_TIME=2;
+	public static final int IS_PART_TIME=1;
 
-	public static final int FULL_TIME=2;
-	public static final int PART_TIME=1;
-	public static final int WORKING_DAY=20;
-	public static final int MAX_HRS=100;
-	EmpWageOOPS(){
-		int empRatePerHrs=20;
-		int totalSalary=0;
-		int salary,empHrs;
-		int totalEmpHrs=0;
-		int totalWorkingDays=0;
-
-		while(totalEmpHrs<=MAX_HRS && totalWorkingDays<=WORKING_DAY) {
-
+	public static int employeeWageComputation(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth){
+		int empHour = 0, totalEmpHours = 0, totalWorkingDays = 0;
+		while(totalEmpHours <=maxHoursPerMonth && totalWorkingDays< numOfWorkingDays){ 
 			int empCheck = (int)Math.floor(Math.random()*10)%3;
-
 			switch(empCheck)	{
-			case FULL_TIME:
-				empHrs = 8;
+			case IS_FULL_TIME:
+				empHour = 8;
 				break;
-			case PART_TIME:
-				empHrs = 4;			
+			case IS_PART_TIME:
+				empHour = 4;
 				break;
 			default:
-				empHrs =0;
+				empHour =0;
 			}
-			salary = empRatePerHrs*empHrs;
-			totalSalary = totalSalary + salary;
-			totalEmpHrs= totalEmpHrs+empHrs;
+			totalEmpHours = totalEmpHours + empHour;
 			totalWorkingDays++;
+			System.out.println("Day : "+totalWorkingDays+" Emp Hours: "+empHour);
 		}
-		System.out.println("Monthly Wage is "+ totalSalary);
+		int totalEmpWage = totalEmpHours * empRatePerHour;
+		System.out.println("Total Emp Wage for " +company+ " is "+totalEmpWage );
+		return totalEmpWage;
 	}
-
-
-	public static void main(String[]args) {
-		EmpWageOOPS obj = new EmpWageOOPS();
+	public static void main(String[] args) {
+		employeeWageComputation("D-mart", 15, 20, 100);
+		employeeWageComputation("Trends", 20, 20, 100);
 	}
 }
