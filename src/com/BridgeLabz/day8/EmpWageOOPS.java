02c07d851c98dbc870d1be2e38bbdb67ea565325
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-27 12:45:09 +0530

    Added:Saved wage for each company

--- src/com/BridgeLabz/day8/EmpWageOOPS.java
+++ src/com/BridgeLabz/day8/EmpWageOOPS.java
@@ -4,7 +4,20 @@ public class EmpWageOOPS {
 	public static final int IS_FULL_TIME=2;
 	public static final int IS_PART_TIME=1;
 
-	public static int employeeWageComputation(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth){
+
+	private String company;
+	private int empRatePerHour;
+	private int numOfWorkingDays;
+	private int maxHoursPerMonth;
+	private int totalEmpWage;
+
+	public EmpWageOOPS(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth){
+		this.company=company;
+		this.empRatePerHour=empRatePerHour;
+		this.numOfWorkingDays=numOfWorkingDays;
+		this.maxHoursPerMonth=maxHoursPerMonth;
+	}	
+	public int wage() {	
 		int empHour = 0, totalEmpHours = 0, totalWorkingDays = 0;
 		while(totalEmpHours <=maxHoursPerMonth && totalWorkingDays< numOfWorkingDays){ 
 			int empCheck = (int)Math.floor(Math.random()*10)%3;
@@ -27,7 +40,12 @@ public class EmpWageOOPS {
 		return totalEmpWage;
 	}
 	public static void main(String[] args) {
-		employeeWageComputation("D-mart", 15, 20, 100);
-		employeeWageComputation("Trends", 20, 20, 100);
+		EmpWageOOPS dmart=new EmpWageOOPS("D-mart", 15, 20, 100);
+		EmpWageOOPS trends=new EmpWageOOPS("Trends", 20, 20, 100);
+
+		dmart.wage();
+		System.out.println("D-mart employee salary"+dmart.wage());
+		trends.wage();
+		System.out.println("Trends employee salary"+trends.wage());
 	}
 }
